bf6e1034c6887393c1cd8aecf9bf45988fb092a2
Nick Perry <nick_perry@clinicalarchitecture.com>
2025-02-20 12:12:17 -0500

    tsconfig, vite.config changes; registering/exporting components correctly

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
 import { router } from './router/router';
-// Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'

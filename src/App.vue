0b7f3c5447995749fab2ac9cb6b1d81969bb92ba
Nick <nicholasjperry1@gmail.com>
2025-02-21 02:30:18 -0500

    nav bar in App.vue; cards in HomeComp; CSS

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,62 @@
 <template lang="pug">
-    div(id="app")
+    VApp(id="app")
+        VAppBar.v-app-bar(:elevation="3")
+            VSpacer
+            .d-flex.flex-row
+                a.nav-item(v-for="item in menuItems" :key="item.name")
+                    span {{ item.name }}
         HomeComponent
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import HomeComponent from './components/HomeComponent.vue';
+
+const menuItems = ref([
+    {
+        name: 'Home',
+        href: '/',
+    },
+    {
+        name: 'Players',
+        href: '#',
+    },
+    {
+        name: 'Results',
+        href: '#',
+    },
+    {
+        name: 'Rules',
+        href: '#',
+    },
+    {
+        name: 'Blog',
+        href: '#',
+    },
+]);
+
+// TODO: Pallette - #557C99, #9DB5C9, #3389CC, #375062
 </script>
 
 <style lang="css">
+#app {
+    background-color: #242424;
+    margin: 0 auto;
+    padding: 2rem;
+}
+
+.v-app-bar {
+    background-image: linear-gradient(to right, #9DB5C9, #557C99) !important;
+}
+
+.nav-item {
+    color: #fff;
+    cursor: pointer;
+    font-family: Inter, system-ui;
+    font-weight: 500;
+    margin-inline-end: 20px;
+    :hover {
+        color: #000;
+    }
+}
 </style>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,9 +1,32 @@
 <template lang="pug">
-    h1 Premier League: Brawl, Coming Soon...
+    VContainer
+        VRow
+            VCol
+                h1(style="color: #fff; font-size: 3rem; text-align: center;") Premier League: Brawl!
+        VRow
+            VCol(cols="6")
+                VCard.v-card(
+                    color="#375062"
+                )
+                    VCardTitle(style="text-align: center") League Description
+                        VCardText(style="color: #fff")
+            VCol(cols="6")
+                VCard.v-card(
+                    color="#375062"
+                )
+                    VCardTitle(style="text-align: center") Sign-Up
+                        VCardText(style="color: #fff")
 </template>
 
 <script setup lang="ts">
+// TODO: Pallette - #557C99, #9DB5C9, #3389CC, #375062
+
 </script>
 
 <style lang="css">
+.v-card {
+    border: 2px solid transparent;
+    box-shadow: 0px 5px 5px black;
+    margin-top: 8px;
+}
 </style>
